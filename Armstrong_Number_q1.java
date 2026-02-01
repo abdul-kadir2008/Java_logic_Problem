b85307cf98e76fd325486c82f4bb3c35a975021b
Abdul Kadir <ka2355504@gmail.com>
2026-02-01 21:06:10 +0530

    Add files via upload

---
+++ Armstrong_Number_q1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Armstrong_Number_q1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your Number ");
+		int num = sc.nextInt();
+		int copy =num;
+		int count = 0;
+	     while (num%10!=0)
+	     { 
+			 count++;
+			 num/=10;
+	     }
+		 num=copy;
+		 int new_Number=1;
+		 int total_Number=0;
+		 while(num%10!=0)
+		 {
+			 new_Number=  1; 
+			 int temp = num%10;
+			for (int i=1;i<=count;i++)
+			{
+				new_Number=temp*new_Number;
+			}
+			total_Number+=new_Number;
+			num/=10;
+		 }
+			 
+		 if(copy==total_Number)
+		{
+			System.out.println("It's Armstrong Number : ");
+		}
+		else
+		{
+			System.out.println("It's not Armstrong Number : ");
+		}
+	}
+}
